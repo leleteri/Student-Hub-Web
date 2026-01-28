c26f73d28989cef54ef1164f2bc27b558e6efe48
leleteri <229750936+leleteri@users.noreply.github.com>
2026-01-28 20:04:08 +0700

    init project

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Flake for web devleopment with react and python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    python = pkgs.python313;
+
+    frontendPackages = [
+      pkgs.nodejs_20
+      pkgs.nodePackages.typescript
+      pkgs.nodePackages.eslint
+      pkgs.nodePackages.prettier
+    ];
+    backendPackages = [
+      (python.withPackages (ps: with ps; [
+        fastapi
+        uvicorn
+        pydantic
+        sqlalchemy
+        alembic
+        asyncpg
+      ]))
+      pkgs.postgresql_16
+    ];
+  in
+  {
+    devShells.${system} = rec {
+      frontend = pkgs.mkShell {
+        packages = [ frontendPackages ];
+      };
+
+      backend = pkgs.mkShell {
+        packages = [ backendPackages ];
+      };
+    
+      default = pkgs.mkShell {
+        packages = [ frontendPackages backendPackages ];
+      };
+    };
+  };
+}
